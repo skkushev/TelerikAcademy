b2b691f274cdf47af2abdd9edcb8e5f850095f25
Steph <skkushev@gmail.com>
2015-01-13 21:52:01 +0200

    repair

--- 02.PrimitiveDataTypesAndVariables/07.QuotesInStrings/QuotesInStrings.cs
+++ 02.PrimitiveDataTypesAndVariables/07.QuotesInStrings/QuotesInStrings.cs
@@ -1,7 +1,7 @@
 ﻿/*Problem 7. Quotes in Strings
 
 Declare two string variables and assign them with following value: The "use" of quotations causes difficulties. 
- Do the above in two different ways - with and without using quoted strings. Print the variables to ensure that their value was correctly defined.*/
+Do the above in two different ways - with and without using quoted strings. Print the variables to ensure that their value was correctly defined.*/
 
 using System;
 
