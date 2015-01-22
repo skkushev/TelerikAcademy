e7f6651558f0b6d0b3bcfabaf89622379756ddf1
Steph <skkushev@gmail.com>
2015-01-22 11:47:50 +0200

    upload

---
+++ 04.ConsoleInputOutput/07.Sum5Numbers/Sum5Numbers.cs
@@ -0,0 +1,26 @@
+﻿/*Problem 7. Sum of 5 Numbers
+
+Write a program that enters 5 numbers (given in a single line, separated by a space), calculates and prints their sum.
+Examples:
+
+numbers     	    sum
+1 2 3 4 5	        15
+10 10 10 10 10	    50
+1.5 3.14 8.2 -1 0	11.84*/
+
+using System;
+
+class Sum5Numbers
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter all numbers on single line, separated by a space");
+        string[] numbers = Console.ReadLine().Split(' ');
+        double sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += Convert.ToDouble(numbers[i]);
+        }
+        Console.WriteLine("Sum of numbers = {0}", sum);
+    }
+}
---
+++ 04.ConsoleInputOutput/08.NumberToN/NumberToN.cs
@@ -0,0 +1,32 @@
+﻿/*Problem 8. Numbers from 1 to n
+
+Write a program that reads an integer number n from the console and prints all the numbers in the interval [1..n], each on a single line.
+Note: You may need to use a for-loop.
+
+Examples:
+
+input	output
+3	    1
+        2
+        3
+5	    1
+        2
+        3
+        4
+        5
+1	    1*/
+
+using System;
+
+    class NumberToN
+    {
+        static void Main()
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
